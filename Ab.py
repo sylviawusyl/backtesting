6d14fcbcd23aa2a53d40e953cc52a20df2c8d470
zishuo <zishuo.wung@gmail.com>
2023-02-19 09:00:44 +0000

    Completed a simple Backtesting Class. Remove data/QQQ.csv garbage at the end.

--- Ab.py
+++ Ab.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import yfinance as yf
 import datetime as dt   
-import csv
+import matplotlib.pyplot as plt
 from abc import abstractmethod, ABCMeta
 class StockData(object):
     def __init__(self, ticker:str):
@@ -69,14 +69,56 @@ print(buy_and_hold.trades)
 
 
 class Portfolio(metaclass=ABCMeta):
-    def __init__(self, principal=1, tradeSize=0.1):
+    def __init__(self, principal, trade_size, prymiding, margin):
         self.principal = principal
-        self.balance = []
-        self.trades = []
-        self.primade = 1
-        self.tradeSize = tradeSize
+        self.prymiding = prymiding
+        self.trade_size = trade_size
+        self.margin = margin
+        #Real account balance should be Cash+Stock-Margin = Total
+        self.balance = pd.DataFrame(columns=['Date','Cash','Stock','Total','Margin'])
+        
     @abstractmethod
-    def buy(strategyEvent):
+    def run_strategy(self, strategy:Strategy, stock_data:StockData):
         pass
-    def sel(strategyEvent):
-        pass
+
+
+class BackTest(Portfolio):
+    def __init__(self, sd:dt.datetime, ed:dt.datetime, principal=1000000, trade_size=1 , prymiding=1):
+        super().__init__(principal, trade_size, prymiding, 0)
+        
+    def run_strategy(self, strategy:Strategy, stock_data:StockData):
+        self.balance['Date'] = stock_data.data.index
+        self.balance['Cash'] = self.principal
+        self.balance['Stock'] = 0
+        self.balance['Total'] = self.principal
+        self.balance['Margin'] = 0
+
+        x = self.balance.iterrows()
+        y = strategy.trades.iterrows()
+        i = next(x, None)
+        j = next(y, None)
+        while i is not None and j is not None:
+            if i[1]['Date'] == j[1]['Date']:
+                if j[1]['Action'] == 'BuyAll':
+                    self.balance.loc[i[0], 'Stock'] = self.trade_size * self.balance.loc[i[0], 'Cash'] / j[1]['Price']
+                    self.balance.loc[i[0], 'Cash'] = self.balance.loc[i[0], 'Cash'] - self.trade_size * self.balance.loc[i[0], 'Cash']
+                    self.balance.loc[i[0], 'Total'] = self.balance.loc[i[0], 'Cash'] + self.balance.loc[i[0], 'Stock'] * j[1]['Price']
+                elif j[1]['Action'] == 'SellAll':
+                    self.balance.loc[i[0], 'Cash'] = self.balance.loc[i[0]-1, 'Stock'] * j[1]['Price']
+                    self.balance.loc[i[0], 'Stock'] = 0
+                    self.balance.loc[i[0], 'Total'] = self.balance.loc[i[0], 'Cash'] + self.balance.loc[i[0], 'Stock'] * j[1]['Price']
+                else:
+                    print('Error: Action not recognized')
+                j = next(y, None)
+            else:
+                #No action on this day, copy the previous day's row value except for Date
+                self.balance.loc[i[0], 'Cash'] = self.balance.loc[i[0]-1, 'Cash']
+                self.balance.loc[i[0], 'Stock'] =  self.balance.loc[i[0]-1, 'Stock']
+                self.balance.loc[i[0], 'Total'] = stock_data.data.loc[i[1]['Date'], 'Close'] * self.balance.loc[i[0], 'Stock'] + self.balance.loc[i[0], 'Cash']
+            i = next(x, None)
+        
+#Test Code, need to remove:
+bt = BackTest(dt.datetime(1998,12,4), dt.datetime(2022,3,2))
+bt.run_strategy(buy_and_hold, qqq2)
+#plot the result
+plt.plot(bt.balance['Date'], bt.balance['Total'])
